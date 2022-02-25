a16deac4670d0070c2aa3d167e9f12d90e464436
jberezow <jberez@live.ca>
2022-02-25 21:24:40 +0100

    Make async happy

--- barney/bot.py
+++ barney/bot.py
@@ -1,11 +1,16 @@
 import discord
 import asyncio
+from discord.ext import commands
 
 from barney.actions.messages import process_message
 from barney.actions.questions import process_question
 from barney.data.api import moe
 
-barney_bot = discord.Client()
+#barney_bot = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+discord.MemberCacheFlags.all()
+barney_bot = commands.Bot("$", intents=intents)
 
 event_dict = {
     '!': process_message,
@@ -16,7 +21,7 @@ event_dict = {
 async def on_message(message):
 
     # Barney won't reply to himself
-    if message.author == barney.user:
+    if message.author == barney_bot.user:
         return
 
     if message.content[0] == '!':
@@ -27,16 +32,23 @@ async def on_message(message):
         loop = asyncio.get_event_loop()
         task = loop.create_task(process_question(message))
 
-@barney_bot.event
 async def sync_users():
     for user in barney_bot.get_all_members():
         id = str(user.id)
         name = str(user.name)
-        loop = asyncio.get_event_loop()
-        task = loop.create_task(moe.upsertUser(id, name))
+        await moe.upsertUser(id,name)
+
+async def sync_channels():
+    for channel in barney_bot.get_all_channels():
+        id = str(channel.id)
+        name = str(channel.name)
+        await moe.upsertChannel(id,name)
 
 @barney_bot.event
 async def on_ready():
     print("{} is now online".format(barney_bot.user.name))
     print("Client user id: {}".format(barney_bot.user.id))
-    await sync_users()
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(sync_users())
+    task2 = loop.create_task(sync_channels())
+    
--- barney/data/api.py
+++ barney/data/api.py
@@ -2,20 +2,20 @@ from gql import gql, Client
 from gql.transport.aiohttp import AsyncTransport
 from barney import GQL_ENDPOINT, HASURA_SECRET
 from aiographql.client import GraphQLClient, GraphQLRequest
+from aiohttp import ClientSession, TCPConnector
 
 class Tavern():
-    moe = GraphQLClient(
+    db_client = GraphQLClient(
         endpoint=GQL_ENDPOINT,
         headers={'x-hasura-admin-secret': HASURA_SECRET}
     )
 
     async def upsertUser(self, id: str, name: str):
-        order_variables = {
-            "id": id,
-            "name": name
-        }
+        connector = TCPConnector(
+            force_close=True, limit=1, enable_cleanup_closed=True
+        )
         order = GraphQLRequest(
-            """
+            query = """
             mutation UpsertUser ($id: String, $name: String) {
                 insert_lads_one(
                     object: {
@@ -27,13 +27,52 @@ class Tavern():
                         update_columns: name
                     }
                 ) 
-                {
+                { 
                     id
                 }
             }
-        """
+        """,
+            variables = {
+                "id": id,
+                "name": name
+            }
         )
-        return await self.moe.query(order, order_variables)
+        session = ClientSession(connector=connector)
+        response = await self.db_client.query(request=order,session=session)
+        await session.close()
+        return response
+    
+    async def upsertChannel(self, id: str, name: str):
+        connector = TCPConnector(
+            force_close=True, limit=1, enable_cleanup_closed=True
+        )
+        order = GraphQLRequest(
+            query = """
+            mutation UpsertChannel ($id: String, $name: String) {
+                insert_channels_one(
+                    object: {
+                        id: $id, 
+                        name: $name
+                    }, 
+                    on_conflict: {
+                        constraint: channels_pkey, 
+                        update_columns: name
+                    }
+                ) 
+                { 
+                    id
+                }
+            }
+        """,
+            variables = {
+                "id": id,
+                "name": name
+            }
+        )
+        session = ClientSession(connector=connector)
+        response = await self.db_client.query(request=order,session=session)
+        await session.close()
+        return response
 
 moe = Tavern()
 
